ca22e28338b0088bf841dc7a77840fbd5060e4ec
TheMatheusDev <apenasmatheus1@gmail.com>
2023-09-27 15:02:16 -0300

    spes: vendorsetup: Rework
    [1] Reorganize
    [2] Rename OF_VIRTUAL_AB_DEVICE to FOX_VIRTUAL_AB_DEVICE
    [3] Drop OF_AB_DEVICE, automatically enabled by FOX_VIRTUAL_AB_DEVICE
    [4] Drop OF_USE_MAGISKBOOT and OF_USE_MAGISKBOOT_FOR_ALL_PATCHES, enabled by default
    [5] Drop FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER, doesn't exist anymore
    [6] Drop OF_NO_TREBLE_COMPATIBILITY_CHECK, treble is the default standard for years
    [7] Drop OF_NO_MIUI_PATCH_WARNING, legacy
    [8] Drop OF_SKIP_MULTIUSER_FOLDERS_BACKUP, doesn't exist anymore
    [9] Drop OF_PATCH_AVB20, this is useful only for A-only devices
    [10] Drop FOX_DISABLE_APP_MANAGER, doesn't exist anymore
    [11] Enable app manager
    [12] Fix TARGET_DEVICE_ALT, it should have only the alt device, not the main
    [13] Added OF_FLASHLIGHT_ENABLE, we don't have a flashlight node, disable it
    [14] Added OF_ADVANCED_SECURITY, disables MTP&ADB during password prompt
    [15] Added FOX_USE_NANO_EDITOR, enables nano
    [16] Rename maintainer
    [17] Fix FOX_VERSION

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,22 +1,23 @@
 #
-# Copyright (C) 2022 The OrangeFox Recovery Project
+#	This file is part of the OrangeFox Recovery Project
+# 	Copyright (C) 2021-2023 The OrangeFox Recovery Project
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
+#	OrangeFox is free software: you can redistribute it and/or modify
+#	it under the terms of the GNU General Public License as published by
+#	the Free Software Foundation, either version 3 of the License, or
+#	any later version.
 #
-# http://www.apache.org/licenses/LICENSE-2.0
+#	OrangeFox is distributed in the hope that it will be useful,
+#	but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#	GNU General Public License for more details.
 #
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# 	This software is released under GPL version 3 or any later version.
+#	See <http://www.gnu.org/licenses/>.
 #
-
+# 	Please maintain this if you use this script or any part of it
+#
 FDEVICE="spes"
-#set -o xtrace
-
 fox_get_target_device() {
 local chkdev=$(echo "$BASH_SOURCE" | grep -w $FDEVICE)
 	if [ -n "$chkdev" ]; then
@@ -32,51 +33,79 @@ if [ -z "$1" -a -z "$FOX_BUILD_DEVICE" ]; then
 fi
 
 if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
-   	export TW_DEFAULT_LANGUAGE="en"
-	export LC_ALL="C"
- 	export ALLOW_MISSING_DEPENDENCIES=true
-	export TARGET_DEVICE_ALT="spes,spesn"
+	# Fox-specific flags
 	export OF_TARGET_DEVICES="spes,spesn"
-	export OF_VIRTUAL_AB_DEVICE=1
-	export OF_AB_DEVICE=1
-	export FOX_RECOVERY_SYSTEM_PARTITION="/dev/block/mapper/system"
-	export FOX_RECOVERY_VENDOR_PARTITION="/dev/block/mapper/vendor"
-	export OF_USE_GREEN_LED=0
-	export OF_FBE_METADATA_MOUNT_IGNORE=1
-
-	export OF_USE_MAGISKBOOT=1
+	export TARGET_DEVICE_ALT="spesn"
+	export FOX_ENABLE_APP_MANAGER=1
+	export FOX_DELETE_AROMAFM=1
+	export FOX_BUGGED_AOSP_ARB_WORKAROUND="1616300800"; # Sun 21 Mar 04:26:40 GMT 2021
+	export OF_QUICK_BACKUP_LIST="/boot;/data;"
 	export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES=1
 	export OF_DONT_PATCH_ENCRYPTED_DEVICE=1
-	export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER=1
 	export OF_NO_TREBLE_COMPATIBILITY_CHECK=1
-	export OF_NO_MIUI_PATCH_WARNING=1
+
+	# Binaries & Tools
 	export FOX_USE_BASH_SHELL=1
 	export FOX_ASH_IS_BASH=1
+	export FOX_USE_NANO_EDITOR=1
 	export FOX_USE_TAR_BINARY=1
 	export FOX_USE_SED_BINARY=1
 	export FOX_USE_XZ_UTILS=1
-	export OF_SKIP_MULTIUSER_FOLDERS_BACKUP=1
-	export OF_QUICK_BACKUP_LIST="/boot;/data;"
-	export OF_PATCH_AVB20=1
-	export FOX_DELETE_AROMAFM=1
-	export FOX_BUGGED_AOSP_ARB_WORKAROUND="1546300800"; # Tuesday, January 1, 2019 12:00:00 AM GMT+00:00
-	export FOX_ENABLE_APP_MANAGER=0
-	export FOX_DISABLE_APP_MANAGER=1
+	export OF_ENABLE_LPTOOLS=1
 
-	# Screen Settings
+	# Version & Maintainer
+	export FOX_VERSION="R11.1"
+	export OF_MAINTAINER="Ascending"
+
+	# MIUI & Custom ROMs
+	export FOX_VIRTUAL_AB_DEVICE=1
+	export OF_NO_MIUI_PATCH_WARNING=1
+
+	# Disable Flashlight & Green LED
+	export OF_FLASHLIGHT_ENABLE=0
+	export OF_USE_GREEN_LED=0
+
+	# Use Magisk v26.3 for the magisk addon
+	export FOX_USE_SPECIFIC_MAGISK_ZIP="$PWD/device/xiaomi/spes/addon/Magisk.v26.3.zip"
+
+	# Security (Disables MTP&ADB during password prompt)
+	export OF_ADVANCED_SECURITY=1
+
+	# Screen settings
 	export OF_SCREEN_H=2400
 	export OF_STATUS_H=110
 	export OF_STATUS_INDENT_LEFT=48
 	export OF_STATUS_INDENT_RIGHT=48
-	export OF_CLOCK_POS=1
-	export OF_HIDE_NOTCH=1
 	export OF_ALLOW_DISABLE_NAVBAR=0
-	
-	# R11.1 Settings
-	export FOX_VERSION="R11.2"
-	export OF_MAINTAINER="Jabiyeff"
+	export OF_HIDE_NOTCH=1
+	export OF_CLOCK_POS=1 # Left & Right
 
-	# let's see what are our build VARs
+	# Maximum permissible splash image size (in kilobytes); do *NOT* increase!
+	export OF_SPLASH_MAX_SIZE=130
+
+	# CCACHE
+	export USE_CCACHE=1
+	export CCACHE_EXEC=/usr/bin/ccache
+
+	CCACHE_DIR="/mnt/ccache"
+
+	if [ -d ${CCACHE_DIR} ];
+	then
+		export CCACHE_DIR=${CCACHE_DIR}
+	else
+		echo "CCACHE Directory/Partition is not mounted at \"${CCACHE_DIR}\""
+		echo "Please edit the CCACHE_DIR build variable or mount the directory."
+	fi
+
+	# Debugging
+	## export FOX_RESET_SETTINGS=0
+	## export FOX_INSTALLER_DEBUG_MODE=1
+
+	# Other..
+	export TW_DEFAULT_LANGUAGE="en"
+	export LC_ALL="C"
+
+	# Let's see what are our build VARs
 	if [ -n "$FOX_BUILD_LOG_FILE" -a -f "$FOX_BUILD_LOG_FILE" ]; then
 		export | grep "FOX" >> $FOX_BUILD_LOG_FILE
 		export | grep "OF_" >> $FOX_BUILD_LOG_FILE
@@ -84,4 +113,4 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
 		export | grep "TW_" >> $FOX_BUILD_LOG_FILE
 	fi
 fi
-
+#
